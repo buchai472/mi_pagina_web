f46bec584c48903319b7cc0a38af8d7b75564cca
Nouhaila Naji <buchainajii@gmail.com>
2025-10-27 13:10:22 +0100

    Actualizar estilos y JavaScript interactivo

---
+++ script.js script.js
@@ -0,0 +1,21 @@
+// Al hacer clic en el título
+document.querySelector("h1").addEventListener("click", () => {
+  alert("¡Hiciste clic en el título! 🌟");
+});
+
+// Al hacer clic en el botón
+document.getElementById("btn").addEventListener("click", () => {
+  alert("Botón pulsado 😊");
+});
+
+// Cambia color del título cada 1.5s
+const h1 = document.querySelector("h1");
+const colors = ["#1a237e", "#ff4081", "#00bfa5", "#fdd835"];
+let i = 0;
+setInterval(() => {
+  h1.style.color = colors[i % colors.length];
+  i++;
+}, 1500);
+
+
+
