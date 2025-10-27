7c44835a1f4c96c2a8155d95333cb4f85891f238
Nouhaila Naji <buchainajii@gmail.com>
2025-10-27 12:36:41 +0100

    Mi primera página web

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("h1").addEventListener("click", () => {
+  alert("¡Hiciste clic en el título!");
+});
+document.getElementById("btn").addEventListener("click", () => {
+  alert("Botón pulsado 😊");
+});
